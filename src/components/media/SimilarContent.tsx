e307ee90b17f02b96c92dc49fbf110464f20c44e
RoyR05 <108044594+RoyR05@users.noreply.github.com>
2026-02-22 20:28:35 -0600

    Updated SearchPage.tsx

--- src/components/media/SimilarContent.tsx
+++ src/components/media/SimilarContent.tsx
@@ -59,12 +59,8 @@ export function SimilarContent({ mediaType, mediaId }: SimilarContentProps) {
         {similar.map((item) => (
           <MediaCard
             key={item.id}
-            id={item.id}
-            title={'title' in item ? item.title : item.name}
-            posterPath={item.poster_path}
+            item={item}
             mediaType={mediaType}
-            releaseDate={'release_date' in item ? item.release_date : item.first_air_date}
-            voteAverage={item.vote_average}
           />
         ))}
       </div>
